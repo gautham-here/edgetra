602393ebd26914b33783184bb0fa37590277ac81
gautham-here <gr0gautham@gmail.com>
2024-10-05 01:51:53 +0530

    Basic Templates, InfoArc and Schema

--- routes.py
+++ routes.py
@@ -4,3 +4,27 @@ from app import app
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/setupD')
+def setupD():
+    return render_template('setupD.html')
+
+@app.route('/setupH')
+def setupH():
+    return render_template('setupH.html')
+
+@app.route('/setupP')
+def setupP():
+    return render_template('setupP.html')
